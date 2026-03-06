aa9202dfa1a749407a090bb40c1cf50942bfd87d
Wryley155 <johnlloydmanilag15@gmail.com>
2026-03-06 17:33:42 +0800

    ENREST2

---
+++ IRIS.Infrastructure/Migrations/20260305100002_pre-finale.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IRIS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class prefinale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
