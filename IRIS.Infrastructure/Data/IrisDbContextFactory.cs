5306c3937bdf23cf1eef092a0c2f0eaf47b1f2df
ErnestoDPuto31 <ernest.agalo31@gmail.com>
2026-04-10 09:47:00 +0800

    DONE DONE

--- IRIS.Infrastructure/Data/IrisDbContextFactory.cs
+++ IRIS.Infrastructure/Data/IrisDbContextFactory.cs
@@ -1,6 +1,11 @@
-﻿using System.Configuration;
+﻿using IRIS.Domain.Entities;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using System.Configuration;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
 
 namespace IRIS.Infrastructure.Data
 {
@@ -10,7 +15,8 @@ namespace IRIS.Infrastructure.Data
         {
             var optionsBuilder = new DbContextOptionsBuilder<IrisDbContext>();
 
-            string connectionString = ConfigurationManager.ConnectionStrings["IrisConnection"].ConnectionString;
+            string connectionString = "Data Source=172.17.9.204,1433;Initial Catalog = IRIS_DB;User ID = sa;Password = cheadmin;Connect Timeout = 30;Encrypt = True; Trust Server Certificate = True;Application Intent = ReadWrite;Multi Subnet Failover = False;Command Timeout = 30";
+
 
             optionsBuilder.UseSqlServer(connectionString);
 
